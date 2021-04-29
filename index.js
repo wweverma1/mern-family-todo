f3149467823999208c1f5f30a466b70a07f3da28
Aditya Verma <wweverma1@gmail.com>
2021-04-29 15:59:20 +0530

    Fixing deployment error

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ const port = process.env.PORT || 5000;
 
 //middleware allow us to send and recieve json
 app.use(cors());
-app.use(express.static('build'));
 app.use(express.json());
+
+/*app.get('/', (req, res) => {
+	res.send('Welcome to Family Todo API');
+});*/
 
 //connecting to the db
 const uri = process.env.ATLAS_URI;
