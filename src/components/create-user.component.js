492210e91a03fbd155a1c7e92cfd11c6f8c4e0b1
Aditya Verma <wweverma1@gmail.com>
2021-04-29 16:37:08 +0530

    Final Project
    Completed Mern based Family ToDo App.

---
+++ src/components/create-user.component.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class CreateUser extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: ''
+    }
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username
+    }
+
+    console.log(user);
+
+    axios.post('http://localhost:5000/users/add', user)
+      .then(res => console.log(res.data));
+
+    this.setState({
+      username: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h3>Add a New Member</h3>
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group"> 
+              <label>Member Name: </label>
+              <input  type="text"
+                  required
+                  className="form-control"
+                  value={this.state.username}
+                  onChange={this.onChangeUsername}
+                  />
+            </div>
+            <div className="form-group">
+              <input type="submit" value="Create User" className="btn btn-primary" />
+            </div>
+          </form>
+        </div>
+        <div>
+          <h3>Avialable Members</h3>
+          <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Member Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              
+            </tr>
+          </tbody>
+        </table>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/components/navbar.component.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+  render() {
+    return ( 
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">Family ToDo List</Link>
+        <div className="collpase navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+          <Link to="/" className="nav-link">Family ToDo</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/create" className="nav-link">Add a ToDo</Link>
+          </li>
+          <li className="navbar-item">
+          <Link to="/user" className="nav-link">Add a Member</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
+    );
+  }
+}
